dc0b84bd00ddc74fc640c4096f72f0d5610c6b57
Josh Mummert Stern <josh.mummertstern@haygroup.com>
2016-05-19 18:48:56 -0400

    scrapping

---
+++ env.js
@@ -0,0 +1,41 @@
+protocol = [ "http", "https" ];
+extension = [ "biz", "com" ];
+domain = [ "haygroup" ];
+
+http://devactivate.haygroup.biz
+https://devintactivate.haygroup.biz
+https://testactivate.haygroup.biz
+https://stagingactivate.haygroup.biz
+https://activate.haygroup.com
+
+var env = [
+  "http://devactivate.haygroup.biz",
+  "https://devintactivate.haygroup.biz",
+  "https://testactivate.haygroup.biz",
+  "https://stagingactivate.haygroup.biz",
+  "https://activate.haygroup.com"
+];
+
+
+devactivate.haygroup.biz
+
+devintactivate.haygroup.biz
+testactivate.haygroup.biz
+stagingactivate.haygroup.biz
+
+activate.haygroup.com
+
+
+
+var alias = [ "devactivate", "devintactivate", "testactivate", "stagingactivate", "activate" ];
+
+function setEnv( alias ){
+  switch ( indesOf(alias) ) {
+    case 0:
+      [break;]
+    case value2:
+      [break;]
+    default:
+      [break;]
+  }
+}
